b6f7cfd5c1f439e00fe8d1e00af6089cb1faf5cf
shoval <shoval123055@gmail.com>
2023-03-01 21:21:29 +0200

    test test

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Devops is great"
+    return "Devops is egreat"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
