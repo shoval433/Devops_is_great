d59a637bb299bacae3b939cc130fe6a14aac51f6
shoval <shoval123055@gmail.com>
2023-03-01 22:14:41 +0200

    order

---
+++ script/calc.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+Version=$(git describe --tags | cut -d '-' -f1 )
+if [ "${Version}" = "" ];then
+Version="1.0.1"
+else
+tag_parts=(${Version//./ })
+# doing the calc on arry 
+((tag_parts[2]++))
+Version="${tag_parts[0]}.${tag_parts[1]}.${tag_parts[2]}"
+fi
+
+# # check the res
+# Version_Test=$(echo $Version | cut -d '.' -f1-2)
+# if [ "${Version_Test}" != "$1" ];then
+# Version="$1.1"
+# fi
+
+echo $Version
